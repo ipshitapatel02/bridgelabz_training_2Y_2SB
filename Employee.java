f212715684b7ba0e18650a37adbba5f24ad0b099
Ipshita <ippu0206@gmail.com>
2025-11-26 22:05:51 +0530

    [ipshita].add add the file

---
+++ Employee.java
@@ -0,0 +1,22 @@
+package class_object;
+
+class Employee {
+    String name;
+    int id;
+    double salary;
+    Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+    void displayDetails() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee ID: " + id);
+        System.out.println("Employee Salary: " + salary);
+    }
+
+    public static void main(String[] args) {
+        Employee emp = new Employee("Marrie", 1, 60000);
+        emp.displayDetails();
+    }
+}
